e90f8baa71abe978e9e3cc2c6f8ee38d2e322ad6
phamhuynh161099 <phamhuynh161099@gmail.com>
2025-07-10 07:13:05 +0700

    hash env

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -6,19 +6,37 @@ import { DatabaseNeoBrustalismService } from './database-neo-brustalism.service'
 @Module({
     imports: [
         // Kết nối MySQL chính
+        // TypeOrmModule.forRootAsync({
+        //     name: 'default', // Tên kết nối mặc định
+        //     imports: [ConfigModule],
+        //     useFactory: (configService: ConfigService) => ({
+        //         type: 'mysql',
+        //         host: configService.get('DB_HOST', 'localhost'),
+        //         port: configService.get('DB_PORT', 3306),
+        //         username: configService.get('DB_USERNAME', 'root'),
+        //         password: configService.get('DB_PASSWORD', ''),
+        //         database: configService.get('DB_NAME', 'nestjs_db'),
+        //         // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        //         synchronize: configService.get('NODE_ENV') !== 'production',
+        //         logging: configService.get('NODE_ENV') === 'development',
+        //         timezone: '+07:00',
+        //     }),
+        //     inject: [ConfigService],
+        // }),
+
         TypeOrmModule.forRootAsync({
             name: 'default', // Tên kết nối mặc định
             imports: [ConfigModule],
             useFactory: (configService: ConfigService) => ({
                 type: 'mysql',
-                host: configService.get('DB_HOST', 'localhost'),
-                port: configService.get('DB_PORT', 3306),
-                username: configService.get('DB_USERNAME', 'root'),
-                password: configService.get('DB_PASSWORD', ''),
-                database: configService.get('DB_NAME', 'nestjs_db'),
+                host: configService.get('103.155.161.244'),
+                port: configService.get('3306'),
+                username: configService.get('dbeaver_user'),
+                password: configService.get('password123'),
+                database: configService.get('neo_brustalism_db'),
                 // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-                synchronize: configService.get('NODE_ENV') !== 'production',
-                logging: configService.get('NODE_ENV') === 'development',
+                synchronize: false,
+                logging: true,
                 timezone: '+07:00',
             }),
             inject: [ConfigService],
